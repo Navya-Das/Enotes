f2dde5d45ed68c333542e91efd81cda6625df66d
Navya-Das <navyadas126@gmail.com>
2025-02-01 13:04:58 +0530

    Add files via upload

---
+++ enotes/src/main/java/com/enotes/repository/NotesRepository.java
@@ -0,0 +1,15 @@
+package com.enotes.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.enotes.entity.Notes;
+import com.enotes.entity.User;
+
+public interface NotesRepository extends JpaRepository<Notes, Integer>{
+
+	public Page<Notes> findByUser(User user,Pageable pageable);
+	
+	
+}
